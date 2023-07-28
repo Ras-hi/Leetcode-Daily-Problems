55bd0a94b2d85621ffa6350703f904bbd82b0619
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2023-07-28 08:07:12 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (43.52%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+         int N = nums.size();
+        vector<int> dp(N);
+        for (int i = 0; i < N; ++i) dp[i] = nums[i];
+        for (int len = 2; len <= N; ++len)
+            for (int i = 0; i <= N - len; ++i)
+                dp[i] = max(nums[i] - dp[i + 1], nums[i + len - 1] - dp[i]);
+        return dp[0] >= 0;
+    }
+};
