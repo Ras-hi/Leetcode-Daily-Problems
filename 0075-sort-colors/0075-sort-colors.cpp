8ef56241866835052a2f5bde794b3c1c47aaf584
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-02-02 15:26:52 +0530

    Time: 5 ms (11.34%), Space: 9.8 MB (17.14%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    /*
+        brute force -merge sort tc:O(nlogn)
+        merge sort:
+        void merge(vector<int>&arr,int low,int mid,int high){
+        vector<int>temp;
+        int left=low;
+        int right=mid+1
+        }
+        void mergesort(vector<int>&arr,int low,int high){
+        if(low>=high) return ;
+        int mid=high-(high-low)/2;
+        mergesort(arr,low,mid);//left half
+        mergesort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+        }
+    */
+    void sortColors(vector<int>& nums) {
+       int cnt0=0,cnt1=0,cnt2=0; 
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0) cnt0++;
+            else if(nums[i]==1) cnt1++;
+            else cnt2++;
+        }
+        for(int i=0;i<cnt0;i++){
+            nums[i]=0;
+        }
+        for(int i=cnt0;i<cnt0+cnt1;i++){
+            nums[i]=1;
+        }
+        for(int i=cnt0+cnt1;i<nums.size();i++){
+            nums[i]=2;
+        }
+        
+    }
+};
