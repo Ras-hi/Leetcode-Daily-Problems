78c80a3e90c08e948f81ed391627bef8aee81663
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-03-29 09:41:03 +0530

    Time: 127 ms (71.71%), Space: 120 MB (62.40%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
-        int mx=INT_MIN;
-        for(auto it:nums){
-            mx=max(mx,it);
-        }
-        int n=nums.size();
-        int freq=0;
-        int start=0;
-        long long ans=0;
-        for(int i=0;i<n;i++){
-            //growing phase
-            if(nums[i]==mx){
-                freq++;
-            }
-            while(freq==k){
-                //shrinking phase
-                if(nums[start]==mx){
-                    freq--;
-                }
-                start++;
-            }
-            ans+=start;
+//         int mx=INT_MIN;
+//         for(auto it:nums){
+//             mx=max(mx,it);
+//         }
+//         int n=nums.size();
+//         int freq=0;
+//         int start=0;
+//         long long ans=0;
+//         for(int i=0;i<n;i++){
+//             //growing phase
+//             if(nums[i]==mx){
+//                 freq++;
+//             }
+//             while(freq==k){
+//                 //shrinking phase
+//                 if(nums[start]==mx){
+//                     freq--;
+//                 }
+//                 start++;
+//             }
+//             ans+=start;
             
+//         }
+//         return ans;
+        int mx=*max_element(nums.begin(),nums.end());
+        int n=nums.size();
+        int cnt=0;
+        long long ans=0;
+        for(int l =0,r=0;r<n;r++){
+            cnt+=(nums[r]==mx);
+            while(cnt>=k){
+                cnt-=(nums[l++]==mx);
+            }
+            ans+=l;
         }
         return ans;
     }
