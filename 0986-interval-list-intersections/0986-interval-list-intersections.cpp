3c969098142d4b4ebe87c13bc7785a840ecf973f
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-03-31 20:17:47 +0530

    Time: 18 ms (89.72%), Space: 22.1 MB (99.40%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
-        vector<vector<int>> ans;
-        int i = 0, j = 0;
-        while(i < size(A) && j < size(B)) {
-			// intersection condition
-            if(A[i][1] >= B[j][0] && A[i][0] <= B[j][1]) 
-                ans.emplace_back(initializer_list<int>{ max(A[i][0], B[j][0]), min(A[i][1], B[j][1]) });
-            A[i][1] < B[j][1] ? i++ : j++;    // increment pointer of list whose interval ended first
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>>ans;
+        int i=0,j=0;
+        while(i<size(firstList) && j<size(secondList)){
+            if(firstList[i][1]>=secondList[j][0] && firstList[i][0]<=secondList[j][1]){
+                ans.emplace_back(initializer_list<int>{max(firstList[i][0],secondList[j][0]),min(firstList[i][1],secondList[j][1])});
+            }
+            firstList[i][1]<secondList[j][1]?i++:j++;
         }
         return ans;
     }
