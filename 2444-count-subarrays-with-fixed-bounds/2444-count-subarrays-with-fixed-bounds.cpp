b10473673ccce77d53a4c0677663803cb77b7be3
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-03-31 17:19:33 +0530

    Time: 96 ms (28.99%), Space: 82.4 MB (90.43%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        long long ans = 0;
-        int j = -1;
-        int prevMinKIndex = -1;
-        int prevMaxKIndex = -1;
-
-        for (int i = 0; i < nums.size(); ++i) {
-          if (nums[i] < minK || nums[i] > maxK)
-            j = i;
-          if (nums[i] == minK)
-            prevMinKIndex = i;
-          if (nums[i] == maxK)
-            prevMaxKIndex = i;
-          // any index k in [j + 1, min(prevMinKIndex, prevMaxKIndex)] can be the
-          // start of the subarray s.t. nums[k..i] satisfies the conditions
-          ans += max(0, min(prevMinKIndex, prevMaxKIndex) - j);
+        long long ans=0;
+        int minkpos=-1;
+        int maxkpos=-1;
+        int cuInd=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<minK || nums[i]>maxK){
+                cuInd=i;
+            }
+            if(nums[i]==minK){
+                minkpos=i;
+            }
+            if(nums[i]==maxK){
+                maxkpos=i;
+            }
+            long long smaller=min(minkpos,maxkpos);
+            long long temp=smaller-cuInd;
+            ans+=(temp<=0)?0:temp;
         }
-
         return ans;
     }
 };
