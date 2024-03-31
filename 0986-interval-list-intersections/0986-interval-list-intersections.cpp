3c7aa442c7b3d65d8625f46944f57afd10525c12
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-03-31 20:08:19 +0530

    Time: 15 ms (96.86%), Space: 22.3 MB (39.55%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
-        
         vector<vector<int>> ans;
         int i = 0, j = 0;
         while(i < size(A) && j < size(B)) {
