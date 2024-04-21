0abf0531ae98c1f10940e004f48101e7a554f93a
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-04-21 11:01:29 +0530

    Time: 3 ms (80.00%), Space: 8.8 MB (40.00%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-    int numberOfSpecialChars(string s) {
-        // set<int>slower;
-        // set<int>supper;
-        // for(auto it : word){
-        //     if(it>='a' && it<='z'){
-        //         slower.insert(it-'a');
-        //     }
-        //     else{
-        //         supper.insert(it-'A');
-        //     }
-        // }
-        // int cnt=0;
-        // for(auto it :slower){
-        //     if(supper.find(it) != slower.end()){
-        //         cnt++;
-        //     }
-        // }
-        // return cnt;
+    int numberOfSpecialChars(string word) {
+        set<int>slower;
+        set<int>supper;
+        for(auto it : word){
+            if(it>='a' && it<='z'){
+                slower.insert(it-'a');
+            }
+            else{
+                supper.insert(it-'A');
+            }
+        }
+        int cnt=0;
+        for(auto it :slower){
+            if(supper.find(it) != supper.end()){
+                cnt++;
+            }
+        }
+        return cnt;
         
-        set<int>s1;
-        set<int>s2;
-        for(auto i:s) {
-            if(i>='a' && i<='z')
-                s1.insert(i-'a');
-            else
-                s2.insert(i-'A');
-        }
-        int c=0;
-        for(auto i:s1) {
-            if(s2.find(i)!=s2.end())
-                c++;
-        }
-        return c;
+        
     }
 };
