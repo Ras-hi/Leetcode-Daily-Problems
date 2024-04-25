e0e21126e806598fc031f0baa0232ddb9c397f01
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-04-25 12:57:57 +0530

    Time: 28 ms (97.90%), Space: 11.5 MB (84.45%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int longestIdealString(string s, int k) {
-        vector<int> cnt(26,0);
-        int ans=0;
-        cnt[s[0]-'0'-49]=1;
-        for(int i=1;i<s.length();i++)
-            {
-            int tmp=0;
-            char t=min('z'-'0'-49,s[i]-'0'-49+k);
-         for(int c=s[i]-'0'-49+1;c<=t;c++)
-             tmp=max(cnt[c]+1,tmp);
-            t=max('a'-'0'-49,s[i]-'0'-49-k);
-            for(int c=s[i]-'0'-49-1;c>=t;c--)
-             tmp=max(cnt[c]+1,tmp);
-        cnt[s[i]-'0'-49]=max(cnt[s[i]-'0'-49]+1,tmp);
+        int mp[26]={};
+        for(auto c: s){
+            int mx=0, x=c-'a';
+            for(int i=max(x-k, 0); i<=min(x+k, 25); i++)
+                mx=max(mx, mp[i]);
+            mp[x]=1+mx;
         }
-        for(char c=0;c<26;c++)
-            ans=max(ans,cnt[c]);
-        return ans;
+        return *max_element(mp, mp+26);
     }
 };
