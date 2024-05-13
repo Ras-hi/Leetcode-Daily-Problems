21d04671781e4f350727e7e654a24a6b162a10ba
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-05-13 22:11:18 +0530

    Time: 13 ms (20.09%), Space: 16.3 MB (34.52%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,45 +1,89 @@
 class Solution {
  public:
   void setZeroes(vector<vector<int>>& matrix) {
-    const int m = matrix.size();
-    const int n = matrix[0].size();
-    bool shouldFillFirstRow = false;
-    bool shouldFillFirstCol = false;
-
-    for (int j = 0; j < n; ++j)
-      if (matrix[0][j] == 0) {
-        shouldFillFirstRow = true;
-        break;
+      const int m=matrix.size();
+      const int n=matrix[0].size();
+      bool firstrow=false;
+      bool firstcol=false;
+      for(int j=0;j<n;j++){
+          if(matrix[0][j]==0){
+              firstrow=true;
+              break;
+          }
       }
-
-    for (int i = 0; i < m; ++i)
-      if (matrix[i][0] == 0) {
-        shouldFillFirstCol = true;
-        break;
+      for(int i=0;i<m;i++){
+          if(matrix[i][0]==0){
+              firstcol=true;
+              break;
+          }
       }
+      // store for all zeroes
+      for(int i=1;i<m;i++){
+          for(int j=1;j<n;j++){
+              if(matrix[i][j]==0){
+                  matrix[0][j]=0;
+                  matrix[i][0]=0;
+              }
+          }
+      }
+      // finally edit values
+      for(int i=1;i<m;i++){
+          for(int j=1;j<n;j++){
+              if(matrix[i][0]==0 || matrix[0][j]==0){
+                  matrix[i][j]=0;
+              }
+          }
+      }
+      if(firstrow){
+          for(int j=0;j<n;j++){
+              matrix[0][j]=0;
+          }
+      }
+      if(firstcol){
+          for(int i=0;i<m;i++){
+              matrix[i][0]=0;
+          }
+      }
+      
+//     const int m = matrix.size();
+//     const int n = matrix[0].size();
+//     bool shouldFillFirstRow = false;
+//     bool shouldFillFirstCol = false;
 
-    // Store the information in the 1st row/col
-    for (int i = 1; i < m; ++i)
-      for (int j = 1; j < n; ++j)
-        if (matrix[i][j] == 0) {
-          matrix[i][0] = 0;
-          matrix[0][j] = 0;
-        }
+//     for (int j = 0; j < n; ++j)
+//       if (matrix[0][j] == 0) {
+//         shouldFillFirstRow = true;
+//         break;
+//       }
 
-    // Fill 0s for the matrix except the 1st row/col
-    for (int i = 1; i < m; ++i)
-      for (int j = 1; j < n; ++j)
-        if (matrix[i][0] == 0 || matrix[0][j] == 0)
-          matrix[i][j] = 0;
+//     for (int i = 0; i < m; ++i)
+//       if (matrix[i][0] == 0) {
+//         shouldFillFirstCol = true;
+//         break;
+//       }
 
-    // Fill 0s for the 1st row if needed
-    if (shouldFillFirstRow)
-      for (int j = 0; j < n; ++j)
-        matrix[0][j] = 0;
+//     // Store the information in the 1st row/col
+//     for (int i = 1; i < m; ++i)
+//       for (int j = 1; j < n; ++j)
+//         if (matrix[i][j] == 0) {
+//           matrix[i][0] = 0;
+//           matrix[0][j] = 0;
+//         }
 
-    // Fill 0s for the 1st col if needed
-    if (shouldFillFirstCol)
-      for (int i = 0; i < m; ++i)
-        matrix[i][0] = 0;
+//     // Fill 0s for the matrix except the 1st row/col
+//     for (int i = 1; i < m; ++i)
+//       for (int j = 1; j < n; ++j)
+//         if (matrix[i][0] == 0 || matrix[0][j] == 0)
+//           matrix[i][j] = 0;
+
+//     // Fill 0s for the 1st row if needed
+//     if (shouldFillFirstRow)
+//       for (int j = 0; j < n; ++j)
+//         matrix[0][j] = 0;
+
+//     // Fill 0s for the 1st col if needed
+//     if (shouldFillFirstCol)
+//       for (int i = 0; i < m; ++i)
+//         matrix[i][0] = 0;
   }
 };
