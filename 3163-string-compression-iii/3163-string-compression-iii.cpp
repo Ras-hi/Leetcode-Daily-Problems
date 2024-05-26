cb775a2ac15d50205d96fcf82c27b469d14c605c
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-05-26 09:54:56 +0530

    Time: 89 ms (40.00%), Space: 28.5 MB (40.00%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.cpp
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     string compressedString(string word) {
-        string comp;
-        int i = 0;
-        int n = word.length();
-
-        while (i < n) {
-            char c = word[i];
-            int count = 0;
-
-            // Count the maximum number of times c repeats, up to 9 times
-            while (i < n && word[i] == c && count < 9) {
+        string comp="";
+        int i=0;
+        int n=word.size();
+        while(i<n){
+            int count =0;
+            char c= word[i];
+            while(i<n && word[i]==c && count <9){
                 ++i;
-                ++count;
+                ++count ;
             }
-
-            // Append the count and the character to comp
-            comp += to_string(count) + c;
+            comp+=to_string(count)+c;
         }
-
         return comp;
     }
 };
