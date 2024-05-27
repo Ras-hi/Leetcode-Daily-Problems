12339a5b4e8b849813ebf8ff4a39a73e4d33844e
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-05-27 09:22:49 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (34.40%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
     int fib(int n) {
-        int zero = 0;
-       int first = 1;
+//         int zero = 0;
+//        int first = 1;
 
 
-       if( n == 0){
-           return 0;
-       }
-       if(n == 1){
-           return 1;
-       }
+//        if( n == 0){
+//            return 0;
+//        }
+//        if(n == 1){
+//            return 1;
+//        }
 
-       int num;
-       int count = 2;
-       for(count ; count <= n ; count++){
+//        int num;
+//        int count = 2;
+//        for(count ; count <= n ; count++){
 
-           num = zero + first ;
-           zero = first;
-           first = num;
+//            num = zero + first ;
+//            zero = first;
+//            first = num;
 
-       }
-        return num;
+//        }
+//         return num;
+        int f1=0;
+        int f2=1;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int f3 ;
+        for(int i=2;i<=n;i++){
+            f3 =f1+f2;
+            f1=f2;
+            f2=f3;
+        }
+        return f3;
     }
 };
